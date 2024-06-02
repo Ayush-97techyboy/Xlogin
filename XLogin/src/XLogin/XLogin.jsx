731cf1057472c24bad40688d1aa32d959331fb06
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-06-02 16:29:41 +0530

    1stf

---
+++ XLogin/src/XLogin/XLogin.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Button, TextField, Container, Typography } from "@mui/material";
+
+const Form = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === "user" && password === "password") {
+      setAuthenticated(true);
+      setMessage(`Welcome ${username}`);
+    } else {
+      setMessage("Invalid credentials!");
+      setAuthenticated(false);
+    }
+  };
+  return (
+    <Container maxWidth="sm">
+      <div style={{ marginTop: "350px" }}>
+        <Typography variant="h4" gutterBottom>
+          Login Page
+        </Typography>
+        <div>
+          {message && !authenticated && (
+            <Typography variant="body1" color="error" sx={{ mb: 3 }}>
+              {message}
+            </Typography>
+          )}
+        </div>
+        {!authenticated && (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <TextField
+                id="username"
+                label="Username"
+                variant="outlined"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+
+            <div style={{ marginTop: "8px" }}>
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2}}
+            >
+              Submit
+            </Button>
+          </form>
+        )}
+        {authenticated && (
+          <Typography variant="body1" sx={{ color: "#056608" }}>
+            Welcome, {username}!
+          </Typography>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default Form;
---
+++ XLogin/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import XLogin from "./XLogin/XLogin";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <XLogin />
+  </React.StrictMode>
+);
